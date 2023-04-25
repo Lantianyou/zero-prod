c3afce50cf480effd59cf7e4d117b798b552a01c
兰天游 <dtlantianyou@gmail.com>
2023-04-25 08:02:51 +0800

    feat: instal deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn index(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("miaomiao").unwrap_or("miaomiao");
+    format!("hi {}", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
